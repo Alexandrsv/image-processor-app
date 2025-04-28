936ce2551a94c8551697045a008da3e3c439d3ba
Александр <zbc@mail.ru>
2025-04-28 16:00:53 +0300

    Фикс роутов и экшена

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Layout } from "@/components/Layout";
 import { ModuleRegistry } from "@/lib/moduleRegistry";
@@ -6,17 +7,20 @@ import { ThemeProvider } from "next-themes";
 
 function App() {
   const FirstModule = ModuleRegistry[0].component;
+
   return (
     <ThemeProvider
       attribute="class"
       defaultTheme="system"
       storageKey="improc-ui-theme"
     >
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <Layout>
           <Routes>
+            {/* стартовая страница               */}
             <Route index element={<FirstModule />} />
 
+            {/* динамические модули              */}
             {ModuleRegistry.map((module) => (
               <Route
                 key={module.id}
@@ -25,11 +29,12 @@ function App() {
               />
             ))}
 
+            {/* fallback на 404 внутри SPA       */}
             <Route path="*" element={<div>Страница не найдена</div>} />
           </Routes>
         </Layout>
       </Router>
-      <Toaster /> {/* Место для отображения уведомлений Shadcn */}
+      <Toaster />
     </ThemeProvider>
   );
 }
