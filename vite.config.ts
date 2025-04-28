1a9370d1701fc4725b59fc352994fb6f00ba818f
Александр <zbc@mail.ru>
2025-04-28 15:24:58 +0300

    Добавить гитхаб экшен для деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const basePath = process.env.BASE_PATH ?? "/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
