077184b914c768ca23dda399416a00f7d115ca41
Александр <zbc@mail.ru>
2025-04-28 13:56:11 +0300

    Добавлены новые зависимости для компонентов Radix UI, обновлены конфигурации ESLint и TypeScript, а также улучшен стиль приложения с помощью Tailwind CSS и tw-animate-css. Обновлены файлы .gitignore и package.json для учета новых зависимостей.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className={"bg-red-500"}>Привет</div>
+      <div className={"bg-red-500 flex justify-center"}>Привет!</div>
+      <Button>Click me</Button>
+      <Input />
     </>
   );
 }
